5d81d556caf0f8f8ee5b5e694d87407769ab30e1
Harishankar Vidhyanandam <53599891+HARISH1211998@users.noreply.github.com>
2023-09-20 10:36:44 +0530

    Update main.tf

--- Import/Testing Folder/API Gateway/main.tf
+++ Import/Testing Folder/API Gateway/main.tf
@@ -7,27 +7,27 @@ terraform {
   }
 }
 
-resource "aws_api_gateway_rest_api" "test-expand-api-gateway" {
-  id = "2jpexdr6y8"
+resource "aws_api_gateway_rest_api" "test-api-gateway" {
+  id = "1234567"
 }
 
-resource "aws_api_gateway_resource" "api_gateway_resource_08xhjv" {
-  id = "08xhjv"
-  parentId = "azhn7n"
-  pathPart = "getliquidity"
-  path = "/bridge/getliquidity"
+resource "aws_api_gateway_resource" "api_gateway_resource_1234" {
+  id = "1234"
+  parentId = "1234"
+  pathPart = "getexample"
+  path = "/example/getexample"
 }
 
-resource "aws_api_gateway_resource" "api_gateway_resource_1p3emd" {
-  id = "1p3emd"
-  parentId = "yuvdh3"
-  pathPart = "getgasprice"
-  path = "/chain/getgasprice"
+resource "aws_api_gateway_resource" "api_gateway_resource_12345" {
+  id = "12345"
+  parentId = "1234a"
+  pathPart = "getexample1"
+  path = "/example/getexample1"
 }
 
-resource "aws_api_gateway_resource" "example_api_gateway_resource_4qlhug" {
-  id = "4qlhug"
-  parentId = "601btu"
-  pathPart = "getmetadata"
-  path = "/nft/getmetadata"
+resource "aws_api_gateway_resource" "example_api_gateway_resource_12345A" {
+  id = "12345A"
+  parentId = "12345a"
+  pathPart = "getexample2"
+  path = "/example/getexample2"
 }
