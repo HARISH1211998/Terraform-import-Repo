99ba249f17e5eea059c44390ca3dc89b4b2e05d5
Harishankar Vidhyanandam <53599891+HARISH1211998@users.noreply.github.com>
2023-09-20 11:52:11 +0530

    Update main.tf

--- Import/Testing Folder/EKS/main.tf
+++ Import/Testing Folder/EKS/main.tf
@@ -2,20 +2,20 @@ provider "aws" {
   region = "ap-southeast-1"  # Change to your desired region
 }
 
-resource "aws_eks_cluster" "ExpandNetwork" {
-  name     = "ExpandNetwork"
-  role_arn = "arn:aws:iam::817309679799:role/eksctl-expandNetwork-cluster-ServiceRole-1L88VH2WXCWWY"  # Replace with your IAM role ARN
+resource "aws_eks_cluster" "Example" {
+  name     = "Example"
+  role_arn = "arn:aws:iam::01234567:role/eksctl-Example-cluster-ServiceRole-1AK1234"  # Replace with your IAM role ARN
 
   vpc_config {
-    subnet_ids          = ["subnet-0ba82a4bb12052bf5", "subnet-078a8e647acac2c5f"]  # Replace with your subnet IDs
-    security_group_ids  = ["sg-078d8cf34a4d937ac "]  # Replace with your security group IDs
+    subnet_ids          = ["subnet-0bdb2344", "subnet-0bdb2344"]  # Replace with your subnet IDs
+    security_group_ids  = ["sg-0bdb2344 "]  # Replace with your security group IDs
     endpoint_public_access = true
     endpoint_private_access = true
   }
 }
 
-resource "aws_eks_node_group" "ExpandNetwork" {
-  cluster_name        = "ExpandNetwork"
-  node_group_name     = "ExpandNetwork"
+resource "aws_eks_node_group" "Example" {
+  cluster_name        = "Example"
+  node_group_name     = "Example"
 }
 
