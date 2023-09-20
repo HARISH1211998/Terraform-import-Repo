7dac5b7988f178c13d101df5cd6d7f81e76b95b1
Harishankar Vidhyanandam <53599891+HARISH1211998@users.noreply.github.com>
2023-09-20 11:50:49 +0530

    Update main.tf

--- Import/Testing Folder/DynamoDB/main.tf
+++ Import/Testing Folder/DynamoDB/main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "ap-southeast-1"  # Change to your desired region
 }
 
-resource "aws_dynamodb_table" "uat_billing_metering" {
-  name           = "uat_billing_metering"
+resource "aws_dynamodb_table" "dynamo_db" {
+  name           = "dynamo_db"
 }
